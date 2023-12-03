643546ebe55b39a2a88ee70560ee5249089ea3cd
Brandon Walsh <bmwalshy@gmail.com>
2023-12-03 13:00:27 -0500

    Day 2 pt 2

--- day2/script.py
+++ day2/script.py
@@ -33,13 +33,34 @@ for line in lines:
     if game_possible:
         result += game_id
 
-print(result)
+print('Part 1: ' + str(result))
 
 
+# Part 2
 
-
-
+result = 0
+for line in lines:
+    # Make list of cubes (i.e. [3 blue, 4 red, 1 red])
+    pulled_cubes = re.split(': |; |, ', line)
+    # Remove first item (Game X: )
+    pulled_cubes.pop(0)
     
+    min_cubes = {
+        'red': 0,
+        'blue': 0,
+        'green': 0 
+    }
 
+    for pulled_cube_info in pulled_cubes:
+        [num_cubes, color] = pulled_cube_info.strip().split(' ')
+        num_cubes = int(num_cubes)
 
+        min_cubes[color] = max(num_cubes, min_cubes[color])
 
+    power = min_cubes['red'] * min_cubes['blue'] * min_cubes['green']
+
+    result += power
+
+print('Part 2: '+ str(result))
+
+# Too lower: 3557
