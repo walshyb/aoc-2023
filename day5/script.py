86f2451f3e49f11e7a67bf093a6ecfda08a2f954
Brandon Walsh <bmwalshy@gmail.com>
2023-12-05 10:48:17 -0500

    Commit bad stuff

--- day5/script.py
+++ day5/script.py
@@ -27,7 +27,6 @@ def fetch(dataset, value):
     return destination_range_start + diff
 
 
-
 usable = None
 for i in range(2, len(lines)):
     line = lines[i]
@@ -70,8 +69,7 @@ for i in range(2, len(lines)):
     usable[range(source_range_start, source_range_start + range_length)] = (destination_range_start, source_range_start, range_length)
 
 
-locations = []
-for seed in seeds:
+def run_it(seed):
     soil = fetch(seed_to_soil, seed)
     fertilizer = fetch(soil_to_fertilizer, soil)
     water = fetch(fertilizer_to_water, fertilizer)
@@ -80,8 +78,70 @@ for seed in seeds:
     humidity = fetch(temp_to_humidity, temp)
     location = fetch(humidity_to_location, humidity)
 
-    locations.append(location)
+    return location
 
-print(min(locations))
 
-# Result 218513636
+def part_1():
+    locations = []
+    for seed in seeds:
+        location = run_it(seed)
+        locations.append(location)
+
+    print(min(locations))
+
+    # Result 218513636
+
+#part_1()
+
+
+    
+
+def part_2():
+    ranges = []
+    min_location = float('inf')
+
+    for seed_start, seed_range in zip(seeds[0::2], seeds[1::2]): 
+        ranges.append(range(seed_start, seed_start + seed_range))
+
+    for seed_range in ranges:
+        # Binary search!!!
+        # that's the key to it all
+        # i missed the trick in pt 1
+        # but we got it now
+
+        #for seed in seed_range:
+        #    min_location = min(min_location, run_it(seed))
+
+        local_min = run_it(seed_range[0])
+        left_ptr = 0
+        right_ptr = len(seed_range) - 1
+        
+        while left_ptr < right_ptr:
+            mid_ptr = (left_ptr + right_ptr) // 2
+            candidate1 = run_it(seed_range[mid_ptr])
+            candidate2 = run_it(seed_range[mid_ptr + 1])
+
+            #print(seed_range[left_ptr], seed_range[mid_ptr], seed_range[right_ptr])
+            candidate3 = run_it(seed_range[right_ptr])
+            candidate4 = run_it(seed_range[left_ptr])
+            print(candidate4, candidate1, candidate3)
+
+            if candidate1 > candidate2:
+                print('go left')
+                left_ptr = mid_ptr + 1
+            else:
+                print('go right')
+                right_ptr = mid_ptr
+        
+            local_min = min(local_min, candidate1, candidate2, candidate3, candidate4)
+
+        min_location = min(min_location, local_min)
+
+        break
+
+    print(min_location)
+
+
+part_2()
+# too high 280278922
+# too high 550136751
